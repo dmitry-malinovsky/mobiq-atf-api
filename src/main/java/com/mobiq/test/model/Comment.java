597e08e310fa2985ed4215d267f251815aa93cb9
dmalinovschi <dmitrii.malinovschii@gmail.com>
2021-02-08 13:01:16 +0200

    - Adjusted models for API

--- src/main/java/com/mobiq/test/model/Comment.java
+++ src/main/java/com/mobiq/test/model/Comment.java
@@ -1,7 +1,13 @@
 package com.mobiq.test.model;
 
 import lombok.Data;
+import org.springframework.scheduling.support.SimpleTriggerContext;
 
 @Data
 public class Comment {
+    private String postId;
+    private String id;
+    private String name;
+    private String email;
+    private String body;
 }
--- src/main/java/com/mobiq/test/model/Post.java
+++ src/main/java/com/mobiq/test/model/Post.java
@@ -4,4 +4,8 @@ import lombok.Data;
 
 @Data
 public class Post {
+    private String userId;
+    private String id;
+    private String title;
+    private String body;
 }
